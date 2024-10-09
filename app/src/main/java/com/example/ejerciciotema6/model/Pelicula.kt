b31f9472f7cfead59a7c2872070e3df46d957824
CarlosJavierValenzue <carlos.valenzuela.j92@gmail.com>
2024-10-09 13:05:58 +0200

    Commit diseño RV y CV

---
+++ app/src/main/java/com/example/ejerciciotema6/model/Pelicula.kt
@@ -0,0 +1,8 @@
+package com.example.ejerciciotema6.model
+
+
+
+class Pelicula (var id:Int,var title:String,var description:String, var poster:Int, var time:Int, var year:Int, var country:String){
+
+
+}
